7e60827cfbf36e96d47d4e61c47233d7cf426c37
isaac martinez <isaacismaelx14@gmail.com>
2022-03-08 09:49:32 -0400

    port folio update

--- src/components/ProjectItem/ProjectItem.vue
+++ src/components/ProjectItem/ProjectItem.vue
@@ -6,7 +6,7 @@
         <div class="card__overlay">
           <div class="card__header">
             <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
-              <path />
+              <path />]ik
             </svg>
             <div class="card__header-text">
               <h3 class="card__title">{{ item.title }}</h3>
